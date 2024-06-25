082fa06a5227025b86b35651dac7e9f157ca345d
murtazatalvadi7 <talvadi.m@northeastern.edu>
2024-06-25 11:19:25 -0400

    change4 25june

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin : 'right' });
 /*==================== typed js ====================*/
 
 var typed = new Typed('.multiple-text', {
-    strings: ['Salesforce Analyst', ' Graduate in Analytics', 'Technology Enthusiast' ],
+    strings: ['Salesforce BSA/Consultant', ' Graduate in Analytics', 'Technology Enthusiast' ],
     typeSpeed: 80,
     backSpeed: 80,
     backDelay: 1000,
